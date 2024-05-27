52e4ceb8a578bddddd1e4a937bf01b1569f58d18
naonom <naonoma@icloud.com>
2024-05-27 16:27:40 +0900

    make footer. edit card design

--- frontend/app/post/page.tsx
+++ frontend/app/post/page.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from "next";
-import { mockData} from "@/constants/mock";
+import { mockData } from "@/constants/mock";
 import { Card } from "@/components/ui/card";
 import Link from "next/link";
 
@@ -7,15 +7,20 @@ const PostListPage: NextPage = () => {
   return (
     <main>
       <h1>Post List Page</h1>
-      <section>
+      <section className="p-2">
         {mockData.map((md) => {
           return (
-            <Link href={`/post/${md.id}`} key={md.id}>
-              <Card>
-                {md.title}
-                {md.body}
-              </Card>
-            </Link>
+            <div className="p-1">
+              <Link href={`/post/${md.id}`} key={md.id}>
+                <Card className="flex justify-between p-2">
+                  <div>
+                    {md.title}
+                    {md.body}
+                  </div>
+                  <div>{md.createdAt.toUTCString()}</div>
+                </Card>
+              </Link>
+            </div>
           );
         })}
       </section>
