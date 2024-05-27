5f6e1a2fb13887bccbb04cbec2c69a83839f71be
nanako-matsumoto <nanako.matsumoto@dena.com>
2024-05-27 14:54:12 +0900

    mockDataの表示、クリックで詳細画面に遷移

--- frontend/app/post/[id]/page.tsx
+++ frontend/app/post/[id]/page.tsx
@@ -1,9 +1,15 @@
-import { NextPage } from "next";
+import { NextPage, NextPageContext } from "next";
 
-const PostDetailPage: NextPage = () => {
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+const PostDetailPage: NextPage<Props> = ({ params }) => {
   return (
     <div>
-      <h1>Post Detail Page</h1>
+      <h1>Post Detail Page {params.id}</h1>
     </div>
   );
 };
--- frontend/app/post/page.tsx
+++ frontend/app/post/page.tsx
@@ -1,9 +1,24 @@
 import { NextPage } from "next";
+import mockData from "@/constans/mock";
+import { Card } from "@/components/ui/card";
+import Link from "next/link";
 
 const PostListPage: NextPage = () => {
   return (
     <div>
       <h1>Post List Page</h1>
+      <>
+        {mockData.map((md) => {
+          return (
+            <Link href={`/post/${md.id}`}>
+              <Card key={md.id}>
+                {md.title}
+                {md.body}
+              </Card>
+            </Link>
+          );
+        })}
+      </>
     </div>
   );
 };
