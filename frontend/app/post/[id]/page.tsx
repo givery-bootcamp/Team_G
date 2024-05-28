28fef8bd07d1c3d06f070a2ff56a989b646e95d4
Asuma Yamada <asuma.a.yamada@dena.com>
2024-05-28 14:21:06 +0900

    fix: add font

--- frontend/app/post/[id]/page.tsx
+++ frontend/app/post/[id]/page.tsx
@@ -14,7 +14,7 @@ const PostDetailPage: NextPage<Props> = ({ params }) => {
     <main className="mx-auto min-h-screen max-w-xl p-1 pt-4">
       <h1 className="mb-4 text-2xl font-bold">Post Detail Page {id}</h1>
       <div className="flex flex-col items-center">
-        <Image src="/noimage.png" alt="Post Image" className="mb-4" width={400} height={400} />
+        <Image src="/images/noimage.png" alt="Post Image" className="mb-4" width={400} height={400} />
         <h2 className="text-xl font-semibold">Post Title</h2>
         <p className="text-md mt-2">
           This is a detailed description of the post content. It provides more context and information about the post.
--- frontend/app/post/page.tsx
+++ frontend/app/post/page.tsx
@@ -17,7 +17,7 @@ const PostListPage: NextPage = async () => {
           return (
             <Link href={`/post/${md.id}`} key={md.id} className="w-full text-center">
               <Card className="mx-auto max-w-fit p-3">
-                <Image src="/noimage.png" alt={md.title} width={300} height={300} />
+                <Image src="/images/noimage.png" alt={md.title} width={300} height={300} />
                 <p className="text-xl font-bold">{md.title}</p>
                 <p className="text-sm">{md.body}</p>
                 <div className="text-xs">{md.createdAt.toLocaleDateString()}</div>
