7735064bb9be99862bf0d8bf918019efbf627990
tsubasa-sakaguchi <tsubasa.sakaguchi@dena.jp>
2024-06-11 12:07:31 +0900

    networkの削除

--- backend/main.go
+++ backend/main.go
@@ -44,7 +44,7 @@ func main() {
 	mux.Handle(path, handler)
 
 	http.ListenAndServe(
-		"localhost:8080",
+		"0.0.0.0:8080",
 		// Use h2c so we can serve HTTP/2 without TLS.
 		h2c.NewHandler(mux, &http2.Server{}),
 	)
