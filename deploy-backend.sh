f38cc1dc03a0ee6cbc9aa43f8cbfd696158596d5
Yuya Isaka <zxwg6329@gmail.com>
2024-06-17 16:10:06 +0900

    fix .sh

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 tag=$1
 
 set -xue
@@ -9,4 +8,4 @@ aws ecr get-login-password --region us-east-1 | docker login --username AWS --pa
 docker build -t dena-training-2024-team-7-backend-prod -f ./docker/backend.prod.Dockerfile .
 docker tag dena-training-2024-team-7-backend-prod:latest 101501319743.dkr.ecr.ap-northeast-1.amazonaws.com/dena-training-2024-team-7-backend-prod:latest
 docker push 101501319743.dkr.ecr.ap-northeast-1.amazonaws.com/dena-training-2024-team-7-backend-prod:latest
-aws ecs update-service --cluster dena-training-2024 --service dena-training-2024-team-7 --task-definition dena-training-2024-team-7:$1
+aws ecs update-service --cluster dena-training-2024 --service dena-training-2024-team-7 --task-definition dena-training-2024-team-7:$tag
