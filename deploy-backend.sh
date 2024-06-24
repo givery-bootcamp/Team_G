66e9cbb353c936a3f4e3f367a15bcc6bfd5ab0a7
aoi-nagatani <aoi.nagatani@dena.com>
2024-06-24 13:53:21 +0900

    chore: Update Dockerfile path in deploy-backend.sh
    Co-authored-by: Yuya Isaka <zxwg6329@gmail.com>
    Co-authored-by: Sakaguchi Tsubasa <kurusunagisa@users.noreply.github.com>

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -5,7 +5,7 @@ set -xue
 
 # Deploy the backend to ecr
 aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 101501319743.dkr.ecr.ap-northeast-1.amazonaws.com
-docker build -t dena-training-2024-team-7-backend-prod -f ./backend.prod.Dockerfile .
+docker build -t dena-training-2024-team-7-backend-prod -f ./docker/backend.prod.Dockerfile .
 docker tag dena-training-2024-team-7-backend-prod:latest 101501319743.dkr.ecr.ap-northeast-1.amazonaws.com/dena-training-2024-team-7-backend-prod:latest
 docker push 101501319743.dkr.ecr.ap-northeast-1.amazonaws.com/dena-training-2024-team-7-backend-prod:latest
 aws ecs update-service --cluster dena-training-2024 --service dena-training-2024-team-7 --task-definition dena-training-2024-team-7:$tag
