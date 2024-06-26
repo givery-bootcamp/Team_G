e4f5f7dc35b5b2c3dd7d08466bf330d88ae6c58e
aoi-nagatani <aoi.nagatani@dena.com>
2024-06-26 16:48:44 +0900

    feat: Add `--force-new-deployment` flag to ECS update-service command
    The code changes in the `deploy-backend.sh` and `deploy-db.sh` scripts modify the ECS update-service command by adding the `--force-new-deployment` flag. This flag ensures that a new deployment is triggered for the specified service in the ECS cluster, dena-training-2024-team-7.
    
    Based on the recent repository commits, the commit message follows the convention of starting with a verb indicating the type of change, followed by a brief description of the change.
    
    Co-authored-by: Sakaguchi Tsubasa <kurusunagisa@users.noreply.github.com>
    Co-authored-by: Yuya Isaka <zxwg6329@gmail.com>

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -8,4 +8,4 @@ aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS
 docker build -t dena-training-2024-team-7-backend-prod -f ./docker/backend.prod.Dockerfile .
 docker tag dena-training-2024-team-7-backend-prod:latest 101501319743.dkr.ecr.ap-northeast-1.amazonaws.com/dena-training-2024-team-7-backend-prod:latest
 docker push 101501319743.dkr.ecr.ap-northeast-1.amazonaws.com/dena-training-2024-team-7-backend-prod:latest
-aws ecs update-service --cluster dena-training-2024 --service dena-training-2024-team-7 --task-definition dena-training-2024-team-7:$tag
+aws ecs update-service --force-new-deployment --cluster dena-training-2024 --service dena-training-2024-team-7 --task-definition dena-training-2024-team-7:$tag
--- docker/deploy-db.sh
+++ docker/deploy-db.sh
@@ -8,4 +8,4 @@ aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS
 docker build --platform linux/amd64 -t dena-training-2024-team-7-db -f ./mongodb.Dockerfile .
 docker tag dena-training-2024-team-7-db:latest 101501319743.dkr.ecr.ap-northeast-1.amazonaws.com/dena-training-2024-team-7-db:latest
 docker push 101501319743.dkr.ecr.ap-northeast-1.amazonaws.com/dena-training-2024-team-7-db:latest
-aws ecs update-service --cluster dena-training-2024 --service dena-training-2024-team-7 --task-definition dena-training-2024-team-7:$tag
+aws ecs update-service --force-new-deployment --cluster dena-training-2024 --service dena-training-2024-team-7 --task-definition dena-training-2024-team-7:$tag
