f3599d73cbd8fe5ae73f04d3602c565b99e87955
nanako-matsumoto <nanako.matsumoto@dena.com>
2024-06-26 11:17:40 +0900

    fbxのサンプルを表示

---
+++ frontend/app/_components/ThreeDFBXViewer.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { OrbitControls, useFBX } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
+interface ThreeDFBXViewerProps {
+  fbxFile: string;
+}
+
+const FBXModel = ({ file }: { file: string }) => {
+  const fbx = useFBX(file);
+  return <primitive object={fbx} scale={10.0} />;
+};
+const ThreeDFBXViewer: React.FC<ThreeDFBXViewerProps> = ({ fbxFile }) => {
+  return (
+    <div className="h-screen w-full">
+      <Canvas camera={{ position: [0, 0, 100], fov: 50 }}>
+        <ambientLight intensity={5.0} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} />
+        <FBXModel file={fbxFile} />
+        <OrbitControls />
+      </Canvas>
+    </div>
+  );
+};
+
+export default ThreeDFBXViewer;
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@/auth";
 import FeatureSection from "./_components/featureSection";
 import HeroSection from "./_components/heroSection";
+import ThreeDFBXViewer from "./_components/ThreeDFBXViewer";
 
 const Home = async () => {
   const session = await auth();
@@ -8,6 +9,7 @@ const Home = async () => {
   return (
     <main className="mx-auto min-h-screen max-w-xl p-1">
       <HeroSection />
+      <ThreeDFBXViewer fbxFile="/test.fbx" />
       <FeatureSection />
     </main>
   );
