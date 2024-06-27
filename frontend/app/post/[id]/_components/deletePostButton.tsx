50b8e76449d31b0e195bc4552333a34ad0afd648
nanako-matsumoto <nanako.matsumoto@dena.com>
2024-06-27 20:03:28 +0900

    削除確認モーダルを作成

--- frontend/app/post/[id]/_components/deletePostButton.tsx
+++ frontend/app/post/[id]/_components/deletePostButton.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogTitle } from "@/components/ui/dialog";
 import { postClient } from "@/lib/connect";
 import { Trash2 } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface Props {
   params: {
@@ -13,8 +15,9 @@ interface Props {
 
 const DeletePostButton = ({ params }: Props) => {
   const router = useRouter();
-  const handleClick = async () => {
-    alert("削除しますか？");
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const handleDelete = async () => {
     try {
       await postClient.deletePost(
         {
@@ -29,13 +32,33 @@ const DeletePostButton = ({ params }: Props) => {
       router.push("/post");
     } catch (e) {
       console.error(e);
+    } finally {
+      setIsDialogOpen(false);
     }
   };
 
+  const handleCancel = () => {
+    setIsDialogOpen(false);
+  };
+
   return (
-    <Button className="bg-white p-2 hover:bg-gray-300" onClick={handleClick}>
-      <Trash2 color="grey" />
-    </Button>
+    <>
+      <Button className="bg-white p-2 hover:bg-gray-300" onClick={() => setIsDialogOpen(true)}>
+        <Trash2 color="grey" />
+      </Button>
+      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <DialogContent>
+          <DialogTitle>確認</DialogTitle>
+          <DialogDescription>本当に削除しますか？</DialogDescription>
+          <DialogFooter>
+            <Button onClick={handleCancel}>キャンセル</Button>
+            <Button className="bg-red-500 text-white" onClick={handleDelete}>
+              削除
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
