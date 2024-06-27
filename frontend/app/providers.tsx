7eca928e23e88730b77e1b86835fa765cbd5a9c9
Asuma Yamada <asuma.a.yamada@dena.com>
2024-06-27 11:54:11 +0900

    feat: add konami command hook

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -1,18 +1,31 @@
 "use client";
 
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { useKonamiCommand } from "@/hooks/useKonamiCommand";
 import { finalTransport } from "@/lib/connect";
 import { TransportProvider } from "@connectrpc/connect-query";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useState } from "react";
 
 const queryClient = new QueryClient();
 
 const Providers: React.FC<PropsWithChildren> = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const onCommandFired = () => {
+    setIsOpen(true);
+  };
+  useKonamiCommand(onCommandFired);
+
   return (
     <SessionProvider>
       <TransportProvider transport={finalTransport}>
-        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+        <QueryClientProvider client={queryClient}>
+          {children}
+          <Dialog open={isOpen} onOpenChange={setIsOpen}>
+            <DialogContent>Test</DialogContent>
+          </Dialog>
+        </QueryClientProvider>
       </TransportProvider>
     </SessionProvider>
   );
