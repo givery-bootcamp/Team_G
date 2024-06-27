3ca019d5d183ef785c356f93507e8a0ad0b18c88
naonom <naonom@icloud.com>
2024-06-27 14:43:29 +0900

    fix review comment

--- frontend/app/post/_components/commentList.tsx
+++ frontend/app/post/_components/commentList.tsx
@@ -8,8 +8,9 @@ const CommentList = ({ commentList }: { commentList: Comment[] }) => {
           <div className="p-2" key={comment.id}>
             <div className="flex justify-between p-1">
               <p className="text-m">{comment.id}</p>
-              if (comment.createdAt)
-              {<p className="text-m text-gray-500">{createDateString(comment.createdAt!.toDate())}</p>}
+              {comment.createdAt && (
+                <p className="text-m text-gray-500">{createDateString(comment.createdAt.toDate())}</p>
+              )}
             </div>
             <p className="text-sm">{comment.body}</p>
           </div>
--- frontend/utils/index.ts
+++ frontend/utils/index.ts
@@ -1,14 +1,9 @@
 export const createDateString = (baseDate: Date): string => {
-  if (baseDate) {
-    const year: string = String(baseDate.getFullYear());
-    const month: string = String(baseDate.getMonth());
-    const day: string = String(baseDate.getDay());
-    const timeH: string = String(baseDate.getHours());
-    const timeM: string = String(baseDate.getMinutes());
-    const timeS: string = String(baseDate.getSeconds());
-    return year + "-" + month + "-" + day + " " + timeH + ":" + timeM + ":" + timeS;
-  } else {
-    console.log("date null");
-    return " ";
-  }
+  const year: string = String(baseDate.getFullYear());
+  const month: string = String(baseDate.getMonth());
+  const day: string = String(baseDate.getDay());
+  const timeH: string = String(baseDate.getHours());
+  const timeM: string = String(baseDate.getMinutes());
+  const timeS: string = String(baseDate.getSeconds());
+  return year + "-" + month + "-" + day + " " + timeH + ":" + timeM + ":" + timeS;
 };
