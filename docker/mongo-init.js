ae75b3dad95ae21267e931dcd5fc2ad50d40f59c
tsubasa-sakaguchi <tsubasa.sakaguchi@dena.jp>
2024-06-27 13:15:28 +0900

    update _id

--- docker/mongo-init.js
+++ docker/mongo-init.js
@@ -43,7 +43,7 @@ db.Post.insert(
    	"title" : "Hello, Hell!",
     "body" : "This is a second test post.",
     "user_id" : "507f1f77bcf86cd799439012",
-    "comments" : [{"user_id": "507f1f77bcf86cd799439011", "body": "This is a comment."}],
+    "comments" : [{"_id": "667cd7db1eb82bf6f8462c9d", "user_id": "507f1f77bcf86cd799439011", "body": "This is a comment."}],
 	"image_url": "https://www.google.com",
     "created_at": {
       "seconds": 0,
