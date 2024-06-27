3dc456a85964da1a199d39bc12b5fb499277203b
Yuya Isaka <zxwg6329@gmail.com>
2024-06-27 14:58:40 +0900

    fix

--- docker/mongo-init.js
+++ docker/mongo-init.js
@@ -38,13 +38,13 @@ db.Post.insert(
   }
 );
 
+// MongoDB内で使用するIDは、ObjectId関数で生成しなければならない
+// 文字列だとエラーになる
 db.Post.insert(
   {
    	"title" : "Hello, Hell!",
     "body" : "This is a second test post.",
     "user_id" : "507f1f77bcf86cd799439012",
-    // MongoDB内で使用するIDは、ObjectId関数で生成しなければならない
-    // 文字列だとエラーになる
     "comments" : [{"_id": ObjectId("667cd7db1eb82bf6f8462c9d"), "user_id": "507f1f77bcf86cd799439011", "body": "This is a comment."}],
 	"image_url": "https://www.google.com",
     "created_at": {
@@ -63,9 +63,7 @@ db.Post.insert(
    	"title" : "Aoi Yuya Basa",
     "body" : "This is a pen",
     "user_id" : "507f1f77bcf86cd799439012",
-    // MongoDB内で使用するIDは、ObjectId関数で生成しなければならない
-    // 文字列だとエラーになる
-    "comments" : [{"_id": ObjectId("667cd7db1eb82bf6f8462c9f"), "user_id": "507f1f77bcf86cd799439011", "body": "mongodb objectID go to hell"}, {"_id": ObjectId("667cd7db1eb82bf6f8462c9g"), "user_id": "507f1f77bcf86cd799439011", "body": "Boku ha kimegao de souitta"}],
+    "comments" : [{"_id": ObjectId("667cd7db1eb82bf6f8462c9f"), "user_id": "507f1f77bcf86cd799439011", "body": "mongodb objectID go to hell"}],
 	"image_url": "https://www.google.com",
     "created_at": {
       "seconds": 0,
